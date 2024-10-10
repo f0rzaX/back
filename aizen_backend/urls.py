6737ff9ba8d50f825ed4832a20dcc55e08c38e4c
f0rzaX <97889756+f0rzaX@users.noreply.github.com>
2024-10-10 21:03:52 +0530

    health

--- aizen_backend/urls.py
+++ aizen_backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 def health_check(request):
-    return HttpResponse("OK", content_type="text/plain")
+    return HttpResponse("I AM ON!", content_type="text/plain")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
-    path("/", health_check), 
+    path("", health_check), 
 ]
