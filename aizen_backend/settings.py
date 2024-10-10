616c48c00b547a235a858d16e50d0606b808846e
f0rzaX <97889756+f0rzaX@users.noreply.github.com>
2024-10-10 21:05:17 +0530

    ALLOWED_HOSTS

--- aizen_backend/settings.py
+++ aizen_backend/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = "django-insecure-$nx^!5r**(=pk7@f^%it1n+f71hh)w_82g%yh@he6g#jhyj#g&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
